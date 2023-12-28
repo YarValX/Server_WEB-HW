1128f907bc8d0bdbd1cee9242165ed46145f825c
YarValX <124351076+YarValX@users.noreply.github.com>
2023-12-28 11:09:33 +0500

    ver 0.8

--- Server.py
+++ Server.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 def start_server(host, port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -6,32 +7,37 @@ def start_server(host, port):
     server_socket.listen(5)
     print(f"Server listening on {host}:{port}")
 
-    while True:
-        client_socket, address = server_socket.accept()
-        print(f"Client {address} connected")
+    clients = []  # Список клиентских сокетов и адресов
 
+    def handle_client(client_socket, address):
         while True:
             data = client_socket.recv(1024).decode()
             if not data:
                 break
             
             print(f"Received message from {address}: {data}")
-            broadcast_message(client_socket, address, data)
+            broadcast_message(client_socket, data)
         
         print(f"Client {address} disconnected")
         client_socket.close()
+        clients.remove((client_socket, address))
 
-def broadcast_message(sender_socket, sender_address, message):
-    for client_socket, client_address in clients:
-        if client_socket != sender_socket:
-            try:
-                client_socket.sendall(message.encode())
-            except socket.error:
-                # Обработка ошибок, если отправка сообщения не удалась
-                pass
+    def broadcast_message(sender_socket, message):
+        for client_socket, client_address in clients:
+            if client_socket != sender_socket:
+                try:
+                    client_socket.sendall(message.encode())
+                except socket.error:
+                    # Обработка ошибок, если отправка сообщения не удалась
+                    pass
 
-clients = []
+    while True:
+        client_socket, address = server_socket.accept()
+        print(f"Client {address} connected")
+        
+        clients.append((client_socket, address))
 
-host = "127.0.0.1"
-port = 12345
-start_server(host, port)
+        thread = threading.Thread(target=handle_client, args=(client_socket, address))
+        thread.start()
+
+start_server("127.0.0.1", 12345)
